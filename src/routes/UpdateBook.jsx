7ac005fc90d3f9b2e2927360c06403aa4def0f72
renan <renan.rodriguesmoura2022@gmail.com>
2023-01-10 09:21:46 -0300

    acessando livro pelo ID

--- src/routes/UpdateBook.jsx
+++ src/routes/UpdateBook.jsx
@@ -1,5 +1,29 @@
-
+import booksFetch from "../Axios/config"
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
 const UpdateBook = () => {
+  const {id} = useParams()
+
+  const [book, setBook] = useState([])
+
+  const getBooks = async () => {
+      try {
+          const response = await booksFetch.get(`/livros/${id}`);
+
+          const data = response.data;
+          console.log(data)
+          setBook(data);
+
+      } catch (error) {
+          console.log(error);
+      }
+  }
+
+  useEffect(() => {
+      getBooks();
+      console.log(book)
+  }, [])
+
   return (
     <div>
       <h1>Atualizar livro</h1>
